42d469d43703bb6fe04d124a0b4adf2647e9973f
jerrf010 <huntingaiden513@gmail.com>
2026-09-25 16:12:39 +0000

    finish lesson7

--- lesson07/lesson7.py
+++ lesson07/lesson7.py
@@ -1,1 +1,15 @@
 # Lesson 7
+
+import random
+
+dc = int(input('DC: '))
+
+if playernum > 20 or playernum < 1:
+    exit()
+
+rannum = random.randint(1,20)
+
+if rannum >= dc:
+    print('You shall pass')
+else: 
+    print('You failed')
